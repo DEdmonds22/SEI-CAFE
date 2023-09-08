ce31cb9e7355593d249d42ea931edeec9d19c0b8
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-08 13:58:45 -0400

    completed task

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,24 +1,8 @@
-// Purpose: Ultimantly we will need to return a JWT from the controller action after the user is added to the database.
-
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
-
-async function create(req, res) {
-    try {
-        // adds the user to the database
-        const user = await User.create(req.body);
-        // token will be a string. After creating the user, a JWT is generated for that user using the `createJWT` function
-        const token = createJWT(user);
-        // Yes, we can use res.json to send back just a string. The client code needs to take this into consideration
-        res.json(token);
-    } catch (err) {
-        // client will check for non-2xx status codes. 400 = Bad Request
-        res.status(400).json(err);
-    }
-};
+const bcrypt = require('bcrypt');
 
 function createJWT(user) {
-    // the `jwt.sign` function is used to create the JWT. It typically includes the user's data as the payload, a secret key for signing the token, and an experation time.
     return jwt.sign(
         {user},
         process.env.SECRET,
@@ -26,66 +10,49 @@ function createJWT(user) {
     );
 };
 
-async function login(req, res) {
-    // we need the try catch block so that if there is an error anywhere in our function, the program keeps going instead of crashing!
+async function create(req, res) {
     try {
+        const user = await User.create(req.body);
+        const token = createJWT(user);
+        res.status(200).json(token);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+};
 
-        // this is a findOne for a single user matching the email that was input into the login form
-        const user = await User.findOne({ email: req.body.email })
-
-        // after the findOne completes, we do this next...
-        .then(foundUser => {
-
-            // note: findOne will not error if there is no user found, instead it just sets foundUser to undefined. Therefore, we have to check if a  user was found.
-            if (foundUser) {
-
-                //if a user was found, we then compare the password they entered in the login form with the password stored in the database. We don't have to input the salt because the hash in the database also contains a key used to rehash a new password for comparison.
-                bcrypt.compare(req.body.password, foundUser.password, (error, result) => {
-                    
-                    // if there was an error in the compare, this runs...
-                    if (error) {
-                        console.log(error);
-                        res.status(400).json(error);
-
-                        //if there was an error, this runs (important: the password being wrong doesn;t count as an error!)
-                    } else {
-
-                        // if the passwords match...
-                        if (result === true) {
-                            
-                            // create a tooken using the info that we found initally
-                            const token = createJWT(foundUser);
-
-                            // sends back a status code of 200 (ok) as well as the token that we just created
-                            res.status(200).json(token);
-
-                            // if the passwords don't match
+async function login(req, res) {
+    try {
+        User.findOne({ email: req.body.email })
+            .then(foundUser => {
+                if (foundUser) {
+                    bcrypt.compare(req.body.password, foundUser.password, (error, result) => {
+                        if (error) {
+                            console.log(error);
+                            res.status(400).json(error);
                         } else {
-
-                            // sends back a status code of 403 (invalid password) as well as a message stating why
-                            res.status(403).json({ error: 'Invalid password!' });
+                            if (result === true) {
+                                const token = createJWT(foundUser);
+                                res.status(200).json(token);
+                            } else {
+                                res.status(403).json({ error: 'Invalid password!' });
+                            };
                         };
-                    };
-                });
-                
-                // if a user was not found matching email provided
-            } else {
-
-                // sends back a 404 error code which means no user was found
-                res.status(404).json({ error: 'User not found '});
-            };
-        });
+                    });
+                } else {
+                    res.status(404).json({ error: 'User not found '});
+                };
+            });
 
         // if any program breaking error happens inside of the try block, this code runs here and the program continues as normal.
-    } catch {
+    } catch(error) {
         res.status(400).json({ error });
     };
 };
 
 function checkToken(req, res) {
-    // req.user will always be there for you when a token is sent
     console.log('req.user', req.user);
-    res.json(req.exp);
+    res.status(200).json(req.exp);
 }
 
 
@@ -94,4 +61,3 @@ module.exports = {
     login,
     checkToken
 };
-// create is in an object because we're gonna have multiple things exported.
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -4,8 +4,6 @@ const BASE_URL = '/api/users';
     /*--- Helper Functions ---*/
 
     async function sendRequest(url, method = 'GET', payload = null) {
-    // Fetch accepts an options object as the 2nd argument
-    // used to include a data payload, set headers, etc.
     const options = { method };
     if (payload) {
         options.headers = { 'Content-Type': 'application/json' };
@@ -26,12 +24,12 @@ const BASE_URL = '/api/users';
 
 export function signUp(userData) {
     return sendRequest(BASE_URL, 'POST', userData);
-    }
+}
 
-    export function login(credentials) {
+export function login(credentials) {
     return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
-    }
+}
 
-    export function checkToken() {
-        return sendRequest(`${BASE_URL}/check-token`);
-    }
+export function checkToken() {
+    return sendRequest(`${BASE_URL}/check-token`);
+}
