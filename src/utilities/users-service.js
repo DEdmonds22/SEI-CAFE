bf12b50ec01b09b2716ca51fb40db2272a8a8c76
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-07 21:48:23 -0400

    saved the token in the brower's local storage

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -7,6 +7,6 @@ import * as usersAPI from './users-api';
 export async function signUp(userData) {
     // delegate the network request code to the users-api.js API module which will ultimately return a JSON Wen Token (JWT)
     const token = await usersAPI.signUp(userData);
-    // returns token up to SignUpForm.js
-    return token;
+    // persist the `token`. When data id persistedm it is preserved beyond the current session or program executed and is typically stored in a way that survives system reboots, program terminations, or other temporary events. setItem sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously. Note: Local Storage only stores and retrieves strings. When saving, the data will automatically be converted to a str, however, you will be responsible for using JSON.parse() to convert the str retrieved from the local storage back into a number, boolean, array, obj, ect.
+    localStorage.setItem('token', token);
 }
