1f8e7b3a0c9454008039f2534652790a4aa7766b
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-07 20:54:04 -0400

    added the user to the database

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,15 @@
 // Purpose: Ultimantly we will need to return a JWT from the controller action after the user is added to the database.
 
-function create(req, res) {
-    res.status(200).json({
-        user: {
-            name: req.body.name,
-            email: req.body.email
-        }
-    });
+const User = require('../../models/user');
+
+async function create(req, res) {
+    try {
+        // adds the user to the database
+        const user = await User.create(req.body);
+    } catch (err) {
+        // client will check for non-2xx status codes. 400 = Bad Request
+        res.status(400).json(err);
+    }
 };
 
 module.exports = {
