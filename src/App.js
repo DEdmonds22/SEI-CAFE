5b122a483d96e93b5527d2515af40ccb1b4293e0
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-07 22:25:07 -0400

    updated the user state

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import AuthPage from './pages/AuthPage';
 import { Routes, Route } from 'react-router-dom';
 import OrderHistoryPage from './pages/OrderHistoryPage';
 import NavBar from './components/NavBar';
+import { getUser } from './utilities/users-service';
 
 function App() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(getUser());
   return (
     <div className="App">
       { user ?
       <>
-        <NavBar />
+        <NavBar user={user} />
         <Routes>
           <Route path='/orders' element={<OrderHistoryPage />} />
           <Route path='/orders/new' element={<NewOrderPage />} />
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -9,4 +9,28 @@ export async function signUp(userData) {
     const token = await usersAPI.signUp(userData);
     // persist the `token`. When data id persistedm it is preserved beyond the current session or program executed and is typically stored in a way that survives system reboots, program terminations, or other temporary events. setItem sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously. Note: Local Storage only stores and retrieves strings. When saving, the data will automatically be converted to a str, however, you will be responsible for using JSON.parse() to convert the str retrieved from the local storage back into a number, boolean, array, obj, ect.
     localStorage.setItem('token', token);
+};
+
+export function getToken() {
+    // getItem returns null if there's no string
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+
+    //obtain the payload of the token
+    const payload = JSON.parse(atob(token.split('.')[1]));
+
+    // a JWT's exp is expressed in secs, not milliseconds, so convert
+    if (payload.exp < Date.now()/1000) {
+        localStorage.removeItem('token');
+        return null;
+    }
+    return token;
 }
+
+// is exported to <APP> to set the user state
+export function getUser() {
+    const token = getToken();
+
+    // if there's a token, return the user in the payload, otherwise return null
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+};
